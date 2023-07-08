a90b5a49a3e3696b77c5d64b67c0192b1b113b78
Crowdbotics <engineering@crowdbotics.com>
2023-07-08 15:26:40 +0000

    Initial commit.

---
+++ backend/threadsapp_dev_99081/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for threadsapp_dev_99081 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'threadsapp_dev_99081.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "threadsapp-dev-99081",
+  url: "https://threadsapp-dev-99081.botics.co",
+  api: "https://threadsapp-dev-99081.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
